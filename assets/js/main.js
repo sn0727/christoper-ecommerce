5bf32048d67022b7d7f6685a4d3928e01fe586d2
sn0727 <alam.sndigitech@gmail.com>
2023-11-23 11:43:08 +0530

    this is project done

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// let menuList = document.querySelectorAll('.top-header-fs-in > ul')[0];
+// let li = menuList.querySelectorAll("li")
+
+// // Get the current page URL
+// let currentPageURL = window.location.pathname;
+
+// console.log(window.location)
+
+// li.forEach(element => {
+//     let a = element.querySelectorAll('a')
+//     a[0].addEventListener('click', function(){
+//         let hrefAtt = a[0].getAttribute("href")
+//         if (currentPageURL === `/${hrefAtt}`) {
+//             this.classList.add("active")
+//         }else{
+//             this.classList.add(" ")
+//         }
+        
+//     })
+// });
